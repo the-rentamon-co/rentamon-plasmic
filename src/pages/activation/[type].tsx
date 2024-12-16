368c6406ca866484abf7cec341732b89e982fd40
Amirhossein Beigi <dev.amirhbeigi@gmail.com>
2024-12-16 15:20:51 +0330

    Update [type].tsx

--- src/pages/activation/[type].tsx
+++ src/pages/activation/[type].tsx
@@ -2,9 +2,9 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../components/plasmic/website_starter/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../../components/plasmic/website_starter/PlasmicGlobalContextsProvider";
 
-import { PlasmicActivationNew } from "../../components/plasmic/website_starter/PlasmicActivationNew";
+import { PlasmicActivationNew } from "../../../components/plasmic/website_starter/PlasmicActivationNew";
 import { useRouter } from "next/router";
 
 function ActivationNew() {
