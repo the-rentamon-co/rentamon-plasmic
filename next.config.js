4f93dffb291336f9c111bd8dd7e1bfd54099a179
parsa khosravi <118853774+parsahvi@users.noreply.github.com>
2025-04-07 12:58:19 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   trailingSlash: true,
+//این بخشو اضافه کنین
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json'
+          }
+        ]
+      }
+    ];
+  }
+  
 };
 
 module.exports = nextConfig;
