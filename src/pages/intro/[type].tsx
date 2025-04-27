a83c162ea4d2e27eb2be1e2b50640e4ef943ac06
parsa khosravi <118853774+parsahvi@users.noreply.github.com>
2025-04-27 18:54:28 +0330

    Update [type].tsx

--- src/pages/intro/[type].tsx
+++ src/pages/intro/[type].tsx
@@ -2,7 +2,7 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../../components/plasmic/website_starter/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../../components/plasmic/website_starter/PlasmicGlobalContextsProvider";
 
 import { PlasmicIntro } from "../../components/plasmic/website_starter/PlasmicIntro";
 import { useRouter } from "next/router";
